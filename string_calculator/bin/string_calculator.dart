0a1735a4fc639acbbec09b502308e0d64d8e300b
Subhojit Pramanik <subhojit.pramanik@ideainfinityit.com>
2025-09-22 22:13:25 +0530

    Empty string returns 0

---
+++ string_calculator/bin/string_calculator.dart
@@ -0,0 +1,5 @@
+import 'package:string_calculator/string_calculator.dart';
+
+void main(List<String> arguments) {
+  print('Hello world: ${StringCalculator().add("0")}!');
+}
---
+++ string_calculator/test/string_calculator_test.dart
@@ -0,0 +1,10 @@
+import 'package:string_calculator/string_calculator.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    final calc = StringCalculator();
+
+    expect(calc.add(''), equals(0));
+  });
+}
